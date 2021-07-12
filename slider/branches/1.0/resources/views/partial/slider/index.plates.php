36d5291c727cf34db69aede7a2753e725abfb083
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-07-12 16:05:09 +0000

    slider >> 1.0.x-dev

--- slider/branches/1.0/resources/views/partial/slider/index.plates.php
+++ slider/branches/1.0/resources/views/partial/slider/index.plates.php
@@ -3,4 +3,16 @@
  * @var Pollen\Partial\PartialTemplateInterface $this
  */
 ?>
-SLIDER
+<?php $this->before(); ?>
+    <div <?php $this->attrs(); ?>>
+        <div class="glide__track" data-glide-el="track">
+            <ul class="glide__slides">
+            <?php foreach ($this->get('slides', []) as $slide) : ?>
+                <li class="glide__slide">
+                    <?php $this->insert('slide', compact('slide')); ?>
+                </li>
+            <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+<?php $this->after();
